48630a9f51a739764f44ec0bdfe824a08366f2d1
somesh3516 <102987739+somesh3516@users.noreply.github.com>
2022-05-03 13:59:56 -0400

    done2

---
+++ hard1.js
@@ -0,0 +1,32 @@
+function checkBrackets(str) {
+    let cRound = 0
+    let cCurly = 0
+    let cSquare = 0
+    for (let char of str) {
+        switch (char) {
+            case "{":
+                cCurly++
+                break
+            case "}":
+                cCurly--
+                break
+            case "(":
+                cRound++
+                break
+            case ")":
+                cRound--
+                break
+            case "[":
+                cSquare++
+                break
+            case "]":
+                cSquare--
+                break
+        }
+
+    }
+return(cCurly==cRound&& cCurly==cSquare)
+    
+}
+console.log(checkBrackets("((([[[{{{This is real}}}]]])))"))
+//by somesh with help from cameron
